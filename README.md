cf9fb250b23f3b8f1904420d21316891b9ef7a3e
Max Willmott <willmott@outlook.com>
2016-05-20 14:56:54 +0100

    Docs

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
-# Project Name
+# PollController.js
 
-[Introduction. What problem does this project solve? Who is it for? Why are we using it at Football Radar?]
+It's just an interval which runs mulitple functions. For example you may want to poll a settings endpoint and a status endpoint every 3 seconds. With a `pollController` we can have both of these functions run of the same loop and that loop is managed in one place. 
+
+# Installing
+`npm install pollcontroller`
+At the moment we are only publishing to npm.
 
 # Getting Started
+```
+import {createPollController} from "pollcontroller"
 
-[Instructions about installation, development guidelines, etc. A few clear bullet points is preferable.]
+const frequentPoller = createPollController();
+
+const clearFrequentActions = frequentPoller.configure({
+    actions: [YOUR_FUNCS],
+    freq: 2000,
+    invokeOnStart: true
+});
+
+frequentPoller.start();
+```
+
+This is the basic usage of the poller. There will now be one interval running ever 2 seconds, running all the actions you have supplied. 
+
+# API
+See `src/index.js` for API docs.
 
 # License
 
-This project
+MIT
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-//torn about classes/factory here
-//we could open source this - Gary ;)
-
 export function createPollController() {
     return Object.create(PollController);
 }
 
 const PollController = {
     _actions: {},
-    _nextActionId: 0,
     _freq: 1000,
     _invokeOnStart: false,
   
+    //@private - used internally only
+    _nextActionId: 0,
+    
+    /**
+     * Modify/set the actiotns, frequency and invokeOnStart settings
+     * @param opts.actions {[function]} - An array of functions to run.
+     * these functions do not get any params and the return value is ignored
+     * @param opts.freq {number} - Default 1000. The frequency of the loop
+     * @param opts.invokeOnStart {bool} - Default false. Invoke the actions on the start of the loop
+     * rather than waiting for the first tick
+     * 
+     * @example
+     * poller.cofigure({
+     *  actions: [() => console.info("tick")],
+     *  freq: 2000,
+     *  invokeOnStart: true
+     * });
+     */
     configure({actions, freq, invokeOnStart}) {
-        //this could use the merge thing from lodash
         const clearFns = actions.reduce((acc, action) => {
             const clearFn = this.addAction(action);
             acc.push(clearFn);
@@ -25,6 +38,12 @@ const PollController = {
         return clearFns;
     },
 
+    /**
+     * Start the loop ticking, invoking the actions if invokeOnStart was passed to the configure function
+     * 
+     * @example
+     * poller.start();
+     */
     start() {
         this.isPolling() && this.stop();
 
@@ -37,15 +56,26 @@ const PollController = {
         }
     },
 
+    /**
+     * Stop the loop.
+     */
     stop() {
         clearInterval(this._interval);
         this._interval = false;
     },
 
+    /**
+     * Is the loop currently ticking
+     * @returns {bool} is polling
+     */
     isPolling() {
         return !!this._interval;
     },
 
+    /**
+     * Add an action to the collection of actions to run per tick
+     * @returns {function} - remove the action from the collection
+     */
     addAction(action) {
         const id = this._nextActionId;
         this._nextActionId++;
@@ -54,10 +84,18 @@ const PollController = {
         return () => this._removeAction(id);
     },
 
+    /**
+     * execute the actions
+     * @private
+     */
     _invokeActions() {
         Object.keys(this._actions).forEach((k) => this._actions[k]());
     },
     
+    /**
+     * Remove an action from the collection. 
+     * @private
+     */
     _removeAction(id){
         delete this._actions[id];
     }
